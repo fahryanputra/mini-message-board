488cfcfac98c5a92d9159d765b853eb1d2eb1080
fahryanputra <fahryanputra@gmail.com>
2024-09-18 10:34:38 +0700

    Display messages

---
+++ express-app/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+// Set EJS template engine
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Routers
+const indexRouter = require("./routes/indexRouter");
+const messageRouter = require("./routes/messageRouter");
+
+app.use("/", indexRouter);
+app.use("/new", messageRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log(
+    `Server running at http://${process.env.HOSTNAME}:${process.env.PORT}`
+  )
+);
